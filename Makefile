2498afb70104e6019a724fa061eb9756c6d238b0
therappha <therappha@gmail.com>
2025-01-11 18:57:30 +0000

    bonus part

--- Makefile
+++ Makefile
@@ -1,25 +1,31 @@
 
 NAME= push_swap
+BONUS= checker
 
 CC= cc
-
 
 CFLAGS= -g -Wall -Wextra -Werror
 INCLUDES= ./includes
 LIBFT_DIR= ./libft
 LIBFT= $(LIBFT_DIR)/libft.a
 SRCS_DIR= ./srcs
+
 SRCS= $(SRCS_DIR)/push_swap.c $(SRCS_DIR)/input_handler.c $(SRCS_DIR)/push_swap_utils.c $(SRCS_DIR)/stack_init.c \
 $(SRCS_DIR)/stack_sorted.c  $(SRCS_DIR)/op/swap.c $(SRCS_DIR)/op/rotate.c $(SRCS_DIR)/op/rev_rotate.c \
 $(SRCS_DIR)/op/push.c $(SRCS_DIR)/sort_three.c $(SRCS_DIR)/debug.c $(SRCS_DIR)/sort.c $(SRCS_DIR)/init_stack_cost.c \
 $(SRCS_DIR)/define_targets.c $(SRCS_DIR)/push_a_to_b.c $(SRCS_DIR)/push_b_to_a.c
 
+BONUS_SRCS= ./bonus/checker.c $(SRCS_DIR)/input_handler.c $(SRCS_DIR)/push_swap_utils.c $(SRCS_DIR)/stack_init.c \
+$(SRCS_DIR)/stack_sorted.c  $(SRCS_DIR)/op/swap.c $(SRCS_DIR)/op/rotate.c $(SRCS_DIR)/op/rev_rotate.c \
+$(SRCS_DIR)/op/push.c $(SRCS_DIR)/debug.c
+
 OBJS=$(SRCS:.c=.o)
+BONUS_OBJS=$(BONUS_SRCS:.c=.o)
 
 all: $(NAME)
 
-$(NAME) : $(OBJS) $(LIBFT) $(FT_PRINTF)
-	$(CC) $(CFLAGS) -I $(INCLUDES) $(OBJS) -L $(LIBFT_DIR) -lft -L $(FT_PRINTF_DIR) -lftprintf -o $(NAME)
+$(NAME) : $(OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) -I $(INCLUDES) $(OBJS) -L $(LIBFT_DIR) -lft -o $(NAME)
 
 %.o : %.c
 	$(CC) $(CFLAGS) -I $(INCLUDES) -c $^ -o $@
@@ -27,12 +33,16 @@ $(NAME) : $(OBJS) $(LIBFT) $(FT_PRINTF)
 $(LIBFT):
 	make -C $(LIBFT_DIR)
 
+bonus: $(BONUS_OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) -I $(INCLUDES) $(BONUS_OBJS) -L $(LIBFT_DIR) -lft -o $(BONUS)
 clean:
 	rm -rf $(OBJS)
+	rm -rf $(BONUS_OBJS)
 	make clean -C $(LIBFT_DIR)
 
 fclean: clean
 	rm -rf $(NAME)
+	rm -rf $(BONUS)
 	make fclean -C $(LIBFT_DIR)
 
 re: fclean all
--- bonus/checker.c
+++ bonus/checker.c
@@ -6,11 +6,14 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 17:32:40 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/01/11 18:20:28 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/01/11 18:56:34 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+static int	perform_ops(t_stack **stack_a, t_stack **stack_b, char *str);
+static void	do_ops(t_stack **stack_a, t_stack **stack_b);
 
 int	main(int ac, char **av)
 {
@@ -28,9 +31,62 @@ int	main(int ac, char **av)
 	if (num_array)
 	{
 		stack_init(&stack_a, num_array, ac -1);
-		get_ops(&stack_a, &stack_b);
+		do_ops(&stack_a, &stack_b);
 		free_stacks(&stack_a, &stack_b);
 	}
 	return (0);
 }
 
+static void	do_ops(t_stack **stack_a, t_stack **stack_b)
+{
+	int		stdin;
+	char	*str;
+
+	stdin = 0;
+	while (1)
+	{
+		str = get_next_line(stdin);
+		if (str == NULL)
+			break ;
+		if (!perform_ops(stack_a, stack_b, str))
+		{
+			free(str);
+			ft_printf("Error\n");
+			return ;
+		}
+		free(str);
+	}
+	if (stack_sorted(stack_a) && (!*stack_b))
+		ft_printf("OK\n");
+	else
+		ft_printf("KO\n");
+}
+
+static int	perform_ops(t_stack **stack_a, t_stack **stack_b, char *str)
+{
+	if (ft_strncmp(str, "sa\n", 3) == 0)
+		sa(stack_a, 0);
+	else if (ft_strncmp(str, "sb\n", 3) == 0)
+		sb(stack_b, 0);
+	else if (ft_strncmp(str, "ss\n", 3) == 0)
+		ss(stack_a, stack_b, 0);
+	else if (ft_strncmp(str, "ra\n", 3) == 0)
+		ra(stack_a, 0);
+	else if (ft_strncmp(str, "rb\n", 3) == 0)
+		rb(stack_b, 0);
+	else if (ft_strncmp(str, "rr\n", 3) == 0)
+		rr(stack_a, stack_b, 0);
+	else if (ft_strncmp(str, "rra\n", 4) == 0)
+		rra(stack_a, 0);
+	else if (ft_strncmp(str, "rrb\n", 4) == 0)
+		rrb(stack_b, 0);
+	else if (ft_strncmp(str, "rrr\n", 4) == 0)
+		rrr(stack_a, stack_b, 0);
+	else if (ft_strncmp(str, "pa\n", 3) == 0)
+		pa(stack_a, stack_b, 0);
+	else if (ft_strncmp(str, "pb\n", 3) == 0)
+		pb(stack_a, stack_b, 0);
+	else
+		return (0);
+	return (1);
+}
